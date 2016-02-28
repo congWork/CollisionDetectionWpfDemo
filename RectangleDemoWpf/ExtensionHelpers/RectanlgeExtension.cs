3450fda0fefa60d91b2eba13d618d2e04b06c3b6
kingcong <conggames@gmail.com>
2016-02-28 15:01:18 -0500

    first commit

---
+++ RectangleDemoWpf/ExtensionHelpers/RectanlgeExtension.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace RectangleDemoWpf.ExtensionHelpers
+{
+    //basic informations about x-axis and y-axis
+    public class RectangleCordinationInfo
+    {
+        public double MinX { get; set; }
+        public double MaxX { get; set; }
+        public double MinY { get; set; }
+        public double MaxY { get; set; }
+    }
+    
+    public static class RectangleExtension
+    {
+        #region Get basic information for this rectangle
+        public static RectangleCordinationInfo GetRectangleCoordinationInfo(Rectangle re)
+        {
+            double left = Canvas.GetLeft(re);
+            double top = Canvas.GetTop(re);
+            double r1X1 = left;
+            double r1X2 = r1X1 + re.Width;
+            double r1Y1 = top;
+            double r1Y2 = r1Y1 + re.Height;
+            return new RectangleCordinationInfo
+            {
+                MinX = r1X1,
+                MaxX = r1X2,
+                MinY = r1Y1,
+                MaxY = r1Y2
+            };
+        }
+        #endregion
+        #region detect containment
+        public static bool ContainsW(this Rectangle r1, Rectangle r2)
+        {
+            int i = 0;
+            var r1Cord = GetRectangleCoordinationInfo(r1);
+            var r2Cord = GetRectangleCoordinationInfo(r2);
+            if (r2Cord.MinX >= r1Cord.MinX && r2Cord.MinX <= r1Cord.MaxX && r2Cord.MaxX >= r1Cord.MinX && r2Cord.MaxX <= r1Cord.MaxX)
+            {
+                i++;
+            }
+            if (r2Cord.MinY >= r1Cord.MinY && r2Cord.MinY <= r1Cord.MaxY && r2Cord.MaxY >= r1Cord.MinY && r2Cord.MaxY <= r1Cord.MaxY)
+            {
+                i++;
+            }
+            if (i == 2)
+            {
+                return true;
+            }
+            return false;
+        }
+        #endregion
+        #region detect adjacency
+        public static bool IsAdjacentTo(this Rectangle r1, Rectangle r2)
+        {
+            var r1Cord = GetRectangleCoordinationInfo(r1);
+            var r2Cord = GetRectangleCoordinationInfo(r2);
+
+            if (r1Cord.MaxX == r2Cord.MinX)
+            {
+                //on the same x, might share the same side
+                if (r2Cord.MinY >= r1Cord.MinY && r2Cord.MaxY <= r1Cord.MaxY)
+                {
+                    return true;
+                }
+            }
+
+            if (r1Cord.MaxX == r2Cord.MaxX)
+            {
+                if (r2Cord.MinX >= r1Cord.MinX)
+                {
+                    return true;
+                }
+            }
+
+            if (r1Cord.MinX == r2Cord.MaxX)
+            {
+                if (r2Cord.MinY >= r1Cord.MinY && r2Cord.MaxY <= r1Cord.MaxY)
+                {
+                    return true;
+                }
+            }
+            if (r1Cord.MinX == r2Cord.MinX)
+            {
+                if (r2Cord.MaxX <= r1Cord.MaxX)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        #endregion
+        #region detect intersection
+        public static bool IntersectsWithW(this Rectangle r1, Rectangle r2)
+        {
+            var r1Cord = GetRectangleCoordinationInfo(r1);
+            var r2Cord = GetRectangleCoordinationInfo(r2);
+
+            double res1 = Math.Abs((r1Cord.MinX + r1Cord.MaxX) / 2 - (r2Cord.MinX + r2Cord.MaxX) / 2);
+            double res2 = (r1Cord.MaxX + r2Cord.MaxX - r1Cord.MinX - r2Cord.MinX) / 2;
+
+            double res3 = Math.Abs((r1Cord.MinY + r1Cord.MaxY) / 2 - (r2Cord.MinY + r2Cord.MaxY) / 2);
+            double res4 = (r1Cord.MaxY + r2Cord.MaxY - r1Cord.MinY - r2Cord.MinY) / 2;
+
+            if (res1 < res2 && res3 < res4)
+                return true;
+            else
+                return false;
+        }
+        #endregion
+    }
+}
---
+++ RectangleDemoWpf/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RectangleDemoWpf.ExtensionHelpers;
+namespace RectangleDemoWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+ 
+    public partial class MainWindow : Window
+    {
+        private bool isDragging;
+        Rectangle r2;
+        public MainWindow()
+        {
+            InitializeComponent();
+            //create a rectangle that is not able to move by mouse
+            CreateARectangle();
+          
+        }
+        public void CreateARectangle()
+        {
+            // Create a blue and a black Brush
+            SolidColorBrush blueBrush = new SolidColorBrush();
+            blueBrush.Color = Colors.Blue;
+            SolidColorBrush blackBrush = new SolidColorBrush();
+            blackBrush.Color = Colors.Black;
+            //second rectangle
+            r2 = new Rectangle();
+            r2.Height = 300;
+            r2.Width = 200;
+
+            r2.StrokeThickness = 1;
+            r2.Stroke = blueBrush;
+         
+            myCanv.Children.Add(r2);
+            Canvas.SetLeft(r2, 400);
+            Canvas.SetTop(r2, 0);
+        }
+        #region detect collision function
+        private void CollisionDetection()
+        {
+            string testResultText = "";
+            if (r2.IntersectsWithW(r1))
+            {
+                testResultText = "Intersection";
+                if (r2.ContainsW(r1))
+                {
+
+                    testResultText = "Containment";
+                }
+            }
+            if (r2.IsAdjacentTo(r1))
+            {
+
+                if (!string.IsNullOrEmpty(testResultText))
+                {
+                    testResultText += " and Adjacent";
+                }
+                else
+                {
+                    testResultText = "Adjacent";
+                }
+            }
+            label.Content = testResultText;
+
+        }
+        #endregion
+        #region make the rectangle move follow the mouse and detect collision when the mouse is up
+        private void r1_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            r1.ReleaseMouseCapture();
+            isDragging = false;
+            CollisionDetection();
+        }
+
+        private void r1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                Point canvPosToWindow = myCanv.TransformToAncestor(this).Transform(new Point(0, 0));
+
+                Rectangle r = sender as Rectangle;
+                var upperlimit = canvPosToWindow.Y + (r.Height / 2);
+                var lowerlimit = canvPosToWindow.Y + myCanv.ActualHeight - (r.Height / 2);
+
+                var leftlimit = canvPosToWindow.X + (r.Width / 2);
+                var rightlimit = canvPosToWindow.X + myCanv.ActualWidth - (r.Width / 2);
+
+
+                var absmouseXpos = e.GetPosition(this).X;
+                var absmouseYpos = e.GetPosition(this).Y;
+                mouseLocationLabel.Content = "Mouse location:" + absmouseXpos + "," + absmouseYpos;
+                if ((absmouseXpos > leftlimit && absmouseXpos < rightlimit)
+                    && (absmouseYpos > upperlimit && absmouseYpos < lowerlimit))
+                {
+                    r.SetValue(Canvas.LeftProperty, e.GetPosition(myCanv).X - (r.Width / 2));
+                    r.SetValue(Canvas.TopProperty, e.GetPosition(myCanv).Y - (r.Height / 2));
+                }
+            }
+        }
+
+        private void r1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            r1.CaptureMouse();
+            isDragging = true;
+        }
+        #endregion
+    }
+}
